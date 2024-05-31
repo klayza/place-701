22b6e44eee1127f38d66ca57ca6bc3ef627e1964
Clayton Wieberg <55328479+klayza@users.noreply.github.com>
2024-05-30 22:36:56 -0700

    Book popup

--- src/routes/(app)/books/+page.svelte
+++ src/routes/(app)/books/+page.svelte
@@ -5,6 +5,42 @@
 	marked.use({ breaks: true });
 	export let data;
 
+	let book_goal = 2;
+	let book_counts = {
+		january: 0,
+		february: 0,
+		march: 0,
+		april: 3,
+		may: 2,
+		june: 0,
+		july: 0,
+		august: 0,
+		september: 0,
+		october: 0,
+		november: 0,
+		december: 0
+	};
+
+	// Get the current month and the previous month
+	let today = new Date();
+	let month_names = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+	let current_month = today.getMonth();
+	let previous_month = current_month === 0 ? 11 : current_month - 1; // handle January case
+
+	// Get the number of books read in the previous month
+	let books_read = book_counts[month_names[previous_month]];
+
+	// Calculate the number of days left in the current month
+	let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+	let days_left = lastDayOfMonth.getDate() - today.getDate();
+
+	const failed = books_read < book_goal;
+
+	// Generate the output statement
+	let fail = `	<div class="border sm:m-8 m-0 my-6 p-6 bg-gray-200">
+	<p class="italic text-md sm:text-xl">${books_read < book_goal ? `Last month I read <span class="text-red-400">${books_read}</span> ${books_read === 1 ? 'book' : 'books'}, so if you see this and are that family member then you get $${(book_goal - books_read) * 100} dollars.` : ''}</p></div>`;
+	let output = `   <p class="italic text-md sm:text-xl">I love to read, and have just begun to read regularly. My goal is to read two books a month. I have setup a deal with family to pay them $100 for every book I fail to complete.</p>	${failed ? fail : ""}`;
+
 	let url = `/data/books.md`;
 	let text = ``;
 
@@ -37,8 +73,7 @@
 	<h1 class="text-xl font-semibold text-center m-auto mt-12 max-w-4xl mb-4">{data.snippet}</h1>
 </div> -->
 <div class="max-w-5xl m-auto text-lg py-24 cursor-default p-8">
-	<p class="italic text-md sm:text-xl">I love to read, and have actually just begun to read regularly. My goal is to read two books a month. I have setup a deal with family to pay them $100 for every book I fail to complete.</p>
-
+	{@html output}
 	<img class="loading m-auto p-12" src="/ico/loading.gif" alt="loading" />
 
 	<div class="md-output all-initial text-sm sm:text-xl mt-20">{@html marked(text)}</div>
@@ -49,9 +84,8 @@
 		background-color: gray !important;
 	}
 
-	/* Example to add margin specifically */
 	.md-output p {
-		margin-bottom: 16px !important;
+		/* margin-bottom: 16px !important; */
 	}
 
 	li {
@@ -62,4 +96,9 @@
 		color: rgb(48, 60, 219);
 	}
 
+	.md-output p {
+		margin: 10px !important;
+		font-size: 19px;
+		line-height: 1 !important;
+	}
 </style>
--- static/data/books.md
+++ static/data/books.md
@@ -2,14 +2,11 @@
 
 ## 2024
 
-**April**
+
 1. The 3 Body Problem *by Cixin Liu*
-2. The Lean Startup *by Eris Ries*
-3. Atomic Habits *by James Clear*
-
-**May**
-
-4. The Beginning of Infinity *by David Deutsch*
-5. <span class="fade-text-light">The First Three Minutes *by Steven Weinberg*</span>
-6. <span class="fade-text-light">Meditations *by Marcus Aurelius (Emperor of Rome)*</span>  
-7. <span class="fade-text-light">Superintellgence *by Nick Bostrom*</span>  
+1. The Lean Startup *by Eris Ries*
+1. Atomic Habits *by James Clear*
+1. The Beginning of Infinity *by David Deutsch*
+1. Superintellgence *by Nick Bostrom*  
+1. <span class="fade-text-light">The First Three Minutes *by Steven Weinberg*</span>
+1. <span class="fade-text-light">Meditations *by Marcus Aurelius (Emperor of Rome)*</span>  
