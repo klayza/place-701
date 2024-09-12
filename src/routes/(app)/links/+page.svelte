089cae30afe19d03248d20e3e10a31ce47198c63
Clayton Wieberg <55328479+klayza@users.noreply.github.com>
2024-09-12 11:50:15 -0700

    links

--- src/routes/(app)/links/+page.svelte
+++ src/routes/(app)/links/+page.svelte
@@ -3,122 +3,123 @@
 	export let data;
 
 	let links = [
-	{
-		url: 'https://projects.kwon.nyc/internet-is-fun/',
-		name: 'Internet is Fun',
-		description: 'A directory of fun websites'
-	},
-	{
-		url: 'https://brianlovin.com/bookmarks/',
-		name: 'Brian Lovin Bookmarks',
-		description: 'A collection of curated bookmarks and resources'
-	},
-	{
-		url: 'https://www.susanrigetti.com/',
-		name: 'Susan Rigetti',
-		description: 'Personal website of Susan Rigetti, a writer and entrepreneur'
-	},
-	{
-		url: 'https://monogram.io/',
-		name: 'Monogram',
-		description: 'A design and development studio creating digital products'
-	},
-	{
-		url: 'https://www.daniel.pizza/',
-		name: "Daniel's Pizza",
-		description: 'A personal website and blog of Daniel, a developer and designer'
-	},
-	{
-		url: 'https://www.lesswrong.com/',
-		name: 'LessWrong',
-		description: 'A community-driven wiki and forum focused on rational thinking and decision-making'
-	},
-	{
-		url: 'https://www.jonway.studio/',
-		name: 'Jonway Studio',
-		description: 'A design studio creating interactive experiences and digital products'
-	},
-	{
-		url: 'https://sarahdrasnerdesign.com/projects/',
-		name: "Sarah Drasner's Projects",
-		description: 'A showcase of projects and experiments by Sarah Drasner, a developer and designer'
-	},
-	{
-		url: 'https://sissonsrowe.com/',
-		name: 'Sissons+Rowe',
-		description: 'Good design'
-	},
-	{
-		url: 'https://andymatuschak.org/',
-		name: 'Andy Matuschak',
-		description: 'Personal website of Andy Matuschak, a researcher and writer on topics like education and technology'
-	},
-	{
-		url: 'https://gracekind.net/',
-		name: 'Gracekind',
-		description: 'A personal site of an AI researcher that has a substantial repository on Ideonomy'
-	},
-	{
-		url: 'https://www.extropic.ai/',
-		name: 'Extropic',
-		description: 'Futuristic + Minimal themed website'
-	},
-	{
-		url: 'https://100r.co/',
-		name: '100r',
-		description: 'A detailed blog by a passionate couple who live on a sailboat'
-	},
-	{
-		url: 'https://localadventurer.com/',
-		name: 'Local Adventurer',
-		description: 'Well documented and thoughtful guides of sites to see'
-	},
-	{
-		url: 'https://redwoodhikes.com/',
-		name: 'David Redwood Hikes',
-		description: 'A taleted writer and experienced hiker\' collection of best spots to hike in the redwood forest'
-	},
-	{
-		url: 'https://www.bookmarked.club/',
-		name: 'Bookmarked Club',
-		description: 'A clean and minimal site to find the best books reccomended by well known people'
-	},
-	{
-		url: 'https://annas-archive.org/',
-		name: 'Anna\'s Archive',
-		description: 'The largest free e-book sharing website. Open-source :)'
-	},
-	{
-		url: 'https://audiobookbay.lu',
-		name: 'Audiobook Bay',
-		description: 'Download almost any audiobook for free'
-	},
-	{
-		url: 'https://immieats.com',
-		name: 'Immieats',
-		description: 'An instant-ramen delivery service with good design'
-	}
-];</script>
+		{
+			url: 'https://www.fxhash.xyz/',
+			name: 'fx(hash)',
+			description: 'Generative art marketplace, very active!'
+		},
+		{
+			url: 'https://projects.kwon.nyc/internet-is-fun/',
+			name: 'Internet is Fun',
+			description: 'A directory of fun websites'
+		},
+		{
+			url: 'https://brianlovin.com/bookmarks/',
+			name: 'Brian Lovin Bookmarks',
+			description: 'A collection of curated bookmarks and resources'
+		},
+		{
+			url: 'https://www.susanrigetti.com/',
+			name: 'Susan Rigetti',
+			description: 'Personal website of Susan Rigetti, a writer and entrepreneur'
+		},
+		{
+			url: 'https://monogram.io/',
+			name: 'Monogram',
+			description: 'A design and development studio creating digital products'
+		},
+		{
+			url: 'https://www.daniel.pizza/',
+			name: "Daniel's Pizza",
+			description: 'A personal website and blog of Daniel, a developer and designer'
+		},
+		{
+			url: 'https://www.lesswrong.com/',
+			name: 'LessWrong',
+			description: 'A community-driven wiki and forum focused on rational thinking and decision-making'
+		},
+		{
+			url: 'https://www.jonway.studio/',
+			name: 'Jonway Studio',
+			description: 'A design studio creating interactive experiences and digital products'
+		},
+		{
+			url: 'https://sarahdrasnerdesign.com/projects/',
+			name: "Sarah Drasner's Projects",
+			description: 'A showcase of projects and experiments by Sarah Drasner, a developer and designer'
+		},
+		{
+			url: 'https://sissonsrowe.com/',
+			name: 'Sissons+Rowe',
+			description: 'Good design'
+		},
+		{
+			url: 'https://andymatuschak.org/',
+			name: 'Andy Matuschak',
+			description: 'Personal website of Andy Matuschak, a researcher and writer on topics like education and technology'
+		},
+		{
+			url: 'https://gracekind.net/',
+			name: 'Gracekind',
+			description: 'A personal site of an AI researcher that has a substantial repository on Ideonomy'
+		},
+		{
+			url: 'https://www.extropic.ai/',
+			name: 'Extropic',
+			description: 'Futuristic + Minimal themed website'
+		},
+		{
+			url: 'https://100r.co/',
+			name: '100r',
+			description: 'A detailed blog by a passionate couple who live on a sailboat'
+		},
+		{
+			url: 'https://localadventurer.com/',
+			name: 'Local Adventurer',
+			description: 'Well documented and thoughtful guides of sites to see'
+		},
+		{
+			url: 'https://redwoodhikes.com/',
+			name: 'David Redwood Hikes',
+			description: "A taleted writer and experienced hiker' collection of best spots to hike in the redwood forest"
+		},
+		{
+			url: 'https://www.bookmarked.club/',
+			name: 'Bookmarked Club',
+			description: 'A clean and minimal site to find the best books reccomended by well known people'
+		},
+		{
+			url: 'https://annas-archive.org/',
+			name: "Anna's Archive",
+			description: 'The largest free e-book sharing website. Open-source :)'
+		},
+		{
+			url: 'https://audiobookbay.lu',
+			name: 'Audiobook Bay',
+			description: 'Download almost any audiobook for free'
+		},
+		{
+			url: 'https://immieats.com',
+			name: 'Immieats',
+			description: 'An instant-ramen delivery service with good design'
+		}
+	];
+</script>
 
 <main class="border-x w-full m-auto 2xl:w-9/12 h-screen">
-	<div
-		class="w-full p-12 pl-8 border-b tracking-widest"
-		style="background-image: linear-gradient(to right, rgb(255, 230, 255), white, white);"
-	>
+	<div class="w-full p-12 pl-8 border-b tracking-widest" style="background-image: linear-gradient(to right, rgb(255, 230, 255), white, white);">
 		<p class="uppercase text-3xl inline-block mr-8">links</p>
 	</div>
 
 	<div class="mx-12 w-fit grid p-3">
 		{#each links as link}
 			<li class="group opacity-80">
-				<a
-					class="text-gray-600 hover:text-gray-900 transition duration-300 ease-in-out list-none text-xl"
-					href={link.url}>{link.name}</a
-				> {@html link.description ? `<i class="ml-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out bg-white"> ` + link.description + `</i>` : ""}
+				<a class="text-gray-600 hover:text-gray-900 transition duration-300 ease-in-out list-none text-xl" href={link.url}>{link.name}</a>
+				{@html link.description ? `<i class="ml-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out bg-white"> ` + link.description + `</i>` : ''}
 			</li>
 		{/each}
 	</div>
-			</main>
+</main>
 
 <style>
 	.w-fit.grid.p-3 li {
@@ -140,5 +141,3 @@
 		background-color: black !important;
 	}
 </style>
-
-
