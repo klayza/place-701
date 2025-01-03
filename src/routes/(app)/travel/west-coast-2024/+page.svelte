5e94476b6293a8fe24de6e2949a8ee4e8e25b7d5
Clayton Wieberg <55328479+klayza@users.noreply.github.com>
2025-01-02 19:42:40 -0700

    west-coast tweak

--- src/routes/(app)/travel/west-coast-2024/+page.svelte
+++ src/routes/(app)/travel/west-coast-2024/+page.svelte
@@ -72,11 +72,11 @@
 
 
 <div class="pt-12 pb-6">
-	<h1 class="text-6xl  px-2 sm:px-0 text-center mt-6 uppercase">Pacific <br>West<br> Coast</h1>
+	<h1 class="text-6xl sm:text-8xl  px-2 sm:px-0 text-center mt-6 uppercase">Pacific <br>West<br> Coast</h1>
 	<p class="max-w-xl px-8 sm:text-2xl text-xl sm:max-w-7xl sm:my-24  m-auto font-medium my-8">4 months spent traveling the west coast in a camper van. From Arizona to L.A, <span class="text-red-200">San Francisco</span>, <span class="text-orange-100">Yosemite Park</span>, Seattle, until <span class="text-blue-100">Banff, Canada</span>.</p>    
 </div>
 
-<div class="max-w-5xl mx-auto pt-0 py-12 px-4 sm:px-8">
+<div class="max-w-screen-2xl mx-auto pt-0 py-12 px-4 sm:px-8">
 	<p class="uppercase mt-6 text-right border-b border-dotted mb-2 font-medium text-gray-300">updated {formatDate(mostRecentModifiedDate)}</p>
 	<!-- <div class="p-6 border my-4 rounded relative mt-4" role="alert">
 		<strong class=""></strong>
@@ -91,17 +91,20 @@
 		</div>
 	</div>
  -->
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-2 ">
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-2 ">
 		{#each Array(totalMdFiles).reverse() as _, i}
-			<a href="/travel/west-coast-2024/w{totalMdFiles - i}" class="block overflow-hidden group relative h-80 no-color static-link">
+			<a href="/travel/west-coast-2024/w{totalMdFiles - i}" class="block overflow-hidden group relative rounded-lg h-40 sm:h-96 no-color static-link">
 				<div class="absolute inset-0 bg-cover bg-center transition-opacity duration-300" style="background-image: url({weekCover(totalMdFiles - i)});">
 					<div class="absolute inset-0 bg-black opacity-50 group-hover:opacity-0 transition-opacity duration-300"></div>
 				</div>
 				<div class="relative h-full flex flex-col justify-center items-center text-white">
-					<h2 class="text-3xl text-center line-clamp-3 text-shadow">{weekTitle(totalMdFiles - i)}</h2>
-					<!-- <p style="font-family: Marker;" class="text-3xl text-center line-clamp-3 text-shadow">{weekTitle(totalMdFiles - i)}</p> -->
-					<p class="text-2xl absolute bottom-2 left-4 font-medium  mb-2  bg-white text-black px-4 rounded-full  ">Week {totalMdFiles - i}</p>
+					<h2 class="text-4xl text-center line-clamp-3 text-shadow">{weekTitle(totalMdFiles - i)}</h2>
+					<p class="text-xl absolute bottom-2 mx-auto font-medium  mb-2 shadow-md  text-white px-4 rounded-full  ">Week {totalMdFiles - i}</p>
 				</div>
+				<!-- <div class="text-lg font-medium sm:text-2xl flex-row flex gap-4 absolute bottom-2 m-auto  mb-2 text-shadow ">
+					<p class="">{weekTitle(totalMdFiles - i)}</p>
+				</div> -->
+
 			</a>
 		{/each}
 	</div>
